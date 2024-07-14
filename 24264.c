96aea3affdda515f05ea6fa8790cd0a2406b00ce
kieranoh <jhoh0317@naver.com>
2024-07-14 17:13:09 +0900

    first commit

---
+++ 24264.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    long long a;
+    scanf("%lld",&a);
+
+    printf("%lld\n2",a*a);
+
+    return 0;
+}
---
+++ 24265.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    long long a;
+    scanf("%lld",&a);
+
+    printf("%lld\n2",a*(a-1)/2);
+
+    return 0;
+}
